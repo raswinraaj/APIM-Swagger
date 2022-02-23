6d2e3c7670b878e8dedbc0cee1bf27d5198e03aa
Aswin Raaj Rajasekaran <aswinraaj.rajasekaran@doverfs.com>
2022-02-23 16:45:35 +0530

    updated backend

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,9 +1,9 @@
 
 terraform{
 backend "azurerm"{   
-   storage_account_name = "__storageaccount_name__"
-   container_name       = "__container_name__"
-    key                  = "__terraformstatefile__"    
-    access_key = "__storageaccount_accesskey__"
+    backend "azurerm" {
+    container_name = "tfstate"
+    key            = "apim.terraform.tfstate"
+  }
 }
 }
